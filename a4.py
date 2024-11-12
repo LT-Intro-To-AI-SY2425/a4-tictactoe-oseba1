36e48c55d6149722622ac9f829a77c46a943aa36
oseba1 <87916222+oseba1@users.noreply.github.com>
2024-11-12 10:56:28 -0600

    updated the tic tac toe

--- a4.py
+++ a4.py
@@ -10,26 +10,54 @@ class TTTBoard:
             represent moves by player 'O' and '*'s are spots no one has yet played on
     """
 
-    pass
+    def __init__(self) -> None:
+        self.board = ["*"] * 9
+
+    def __str__(self):
+        return f"{self.board[0]} | {self.board[1]} | {self.board[2]}\n---------\n{self.board[3]} | {self.board[4]} | {self.board[5]}\n---------\n{self.board[6]} | {self.board[7]} | {self.board[8]}"
+
+    def make_move(self, char, index):
+        if self.board[index] == "*":
+            self.board[index] = char
+        else:
+            return None
+        
+    def clear(self):
+        self.board = ["*"] * 9
+
+    def has_won(self, char):
+
+        # check horizontal
+        for i in range(0,3):
+            if self.board[0 + i * 3] == self.board[1 + i * 3] == self.board[2 + i * 3] and self.board[0 + i * 3] == char:
+                return True
+        
+        # check vertical
+        for i in range(0,3):
+            if self.board[0 + i] == self.board[3 + i] == self.board[6 + i] and self.board[0 + i] == char:
+                return True
+            
+        # check diagonal
+        if self.board[0] == self.board[4] == self.board[8] and self.board[0] == char:
+                return True
+        if self.board[2] == self.board[4] == self.board[6] and self.board[2] == char:
+                return True
+        
+        return False
+
+    def game_over(self):
+        
+        if self.has_won("X") or self.has_won("O") or "*" not in self.board:
+            return True
+
+        
+        return False
+
+
 
 
 def play_tic_tac_toe() -> None:
     """Uses your class to play TicTacToe"""
-
-    def is_int(maybe_int: str):
-        """Returns True if val is int, False otherwise
-
-        Args:
-            maybe_int - string to check if it's an int
-
-        Returns:
-            True if maybe_int is an int, False otherwise
-        """
-        try:
-            int(maybe_int)
-            return True
-        except ValueError:
-            return False
 
     brd = TTTBoard()
     players = ["X", "O"]
@@ -37,15 +65,30 @@ def play_tic_tac_toe() -> None:
 
     while not brd.game_over():
         print(brd)
-        move: str = input(f"Player {players[turn]} what is your move? ")
 
-        if not is_int(move):
-            raise ValueError(
-                f"Given invalid position {move}, position must be integer between 0 and 8 inclusive"
-            )
+        while True:
+            try:
+                move: str = input(f"Player {players[turn]} what is your move? ")
+                #assert int(move) == int
 
-        if brd.make_move(players[turn], int(move)):
-            turn = not turn
+                if int(move) not in (0,1,2,3,4,5,6,7,8):
+                    raise ValueError
+
+                if brd.board[int(move)] != "*":
+                    raise ValueError
+                    
+            except ValueError:
+                print("Enter a valid index.")
+                continue
+            else:
+                break
+        
+        brd.make_move(players[turn], int(move))
+
+        if turn == 0:
+            turn = 1
+        elif turn == 1:
+            turn = 0
 
     print(f"\nGame over!\n\n{brd}")
     if brd.has_won(players[0]):
@@ -86,7 +129,7 @@ if __name__ == "__main__":
     assert brd.has_won("O") == True
     assert brd.game_over() == True
 
-    print("All tests passed!")
+    #print("All tests passed!")
 
     # uncomment to play!
-    # play_tic_tac_toe()
+    play_tic_tac_toe()
